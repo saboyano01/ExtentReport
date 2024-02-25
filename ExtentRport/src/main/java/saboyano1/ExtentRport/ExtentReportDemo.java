710ec062832c976f92a09b0cb659aeafdae77542
dell <dell@DESKTOP-TOPG9LE.mshome.net>
2024-02-25 11:13:19 +0000

    Initial Commit5

--- ExtentRport/src/main/java/saboyano1/ExtentRport/ExtentReportDemo.java
+++ ExtentRport/src/main/java/saboyano1/ExtentRport/ExtentReportDemo.java
@@ -1,5 +1,35 @@
 package saboyano1.ExtentRport;
 
+//import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+//import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 public class ExtentReportDemo {
+	
+	
+//	@BeforeTest
+//	public void config()
+//	{
+////		String path = System.getProperty("user.dir")+"\\reports\\index.html";
+////		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+////		reporter.config().setReportName("Web Automation Results");
+////		reporter.config().setDocumentTitle("Test Results");
+////		
+//	}
+//    
+
+//	@Test
+//	public void initialDemo()
+////	{
+////		extent.createTest("Initial Demo");
+////		System.setProperty("webdriver.chrome.driver","C://chromedriver.exe");
+////		WebDriver  driver = new ChromeDriver();
+////		driver.get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java/3.141.59");
+////		System.out.println(driver.getTitle());
+////		extent.flush();
+////		
+//	}		
 
 }
